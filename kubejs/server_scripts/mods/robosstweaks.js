9c2e9e5c00a1cee48dfcd55e18f7ca8831da07d1
R0B0TB0SS <74618936+robotboss001@users.noreply.github.com>
2024-01-02 17:41:57 +0100

    02/01/24 17:41

--- kubejs/server_scripts/mods/robosstweaks.js
+++ kubejs/server_scripts/mods/robosstweaks.js
@@ -1,21 +1,3 @@
 onEvent('recipes', e => {
-    
-    e.shaped(Item.of('custommachinery:custom_machine_item', '{machine:"robosstweaks:creative_fabricator"}'), [
-        ' S ',
-        ' O ',
-        ' A '
-      ], {
-        A: 'robosstweaks:creative_catalyst',
-        O: 'computercraft:monitor_normal',
-        S: 'avaritia:infinity_catalyst'
-      })
-    
-      e.shaped('8x robosstweaks:creative_catalyst', [
-        'SSS',
-        'SOS',
-        'SSS'
-      ], {
-        S: 'ae2:controller',
-        O: 'avaritia:infinity_block'
-      })
+  
 })
--- kubejs/server_scripts/unify.js
+++ kubejs/server_scripts/unify.js
@@ -75,15 +75,6 @@ onEvent('recipes', e => {
         })
 
 
-
-
-
-
-
-
-
-
-
         e.replaceInput(
             "minecraft:lava_bucket",
             Ingredient.of([Item.of("chickens:liquid_egg", '{id:"minecraft:lava"}'), "minecraft:lava_bucket"]).toJson()
